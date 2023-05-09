eee96a9872c8bb622345c15079196a82c983850a
Naz <121558403+MNazran@users.noreply.github.com>
2023-05-09 11:46:44 +0800

    Add files via upload

---
+++ socialmedia-MERN/server/controllers/auth.js
@@ -0,0 +1,68 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    // 2. create a new user * should check if user already exist
+    // hashing the password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save(); //save the user to the database
+    res.status(201).json(savedUser); //send the response
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // 1. check if user exists
+    const user = await User.findOne({ email: email });
+
+    // if user doesn't exist, return error
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' });
+
+    // 2. if user exists, check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // if password is incorrect, return error
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials.' });
+
+    // 3. if password is correct, create the tokens
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    delete user.password;
+
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
